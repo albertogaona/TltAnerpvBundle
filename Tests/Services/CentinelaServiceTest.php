b9bb30f9edcd8704423a32d38e375a7d8c7ae3ab
Alberto Gaona <alberto.gaona@tilatina.com>
2014-01-10 08:58:53 -0600

    Test

---
+++ Tests/Services/CentinelaServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tlt\AnerpvBundle\Test\Services;
+
+class CentielaServiceTest extends \PHPUnit_Framework_TestCase
+{
+	public function testReportarEmergencia()
+	{
+		$this->assertTrue(true);
+	}
+	
+	public function testConsultarStatus()
+	{
+		$this->assertTrue(true);
+	}
+	
+}
+
+?>
